7f9f2f94db983ca7f3810b0882e82c4ea347a538
Murillo Santos <murillo.lds01@gmail.com>
2024-10-03 14:55:31 -0300

    feat: create detalhes component

---
+++ src/app/pages/detalhes/detalhes.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FuncionarioService } from '../../services/funcionario.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Funcionarios } from '../../models/Funcionarios';
+
+@Component({
+  selector: 'app-detalhes',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalhes.component.html',
+  styleUrl: './detalhes.component.css'
+})
+export class DetalhesComponent implements OnInit{
+
+  funcionario!: Funcionarios;
+  id!:number
+
+  constructor(private readonly funcionarioService: FuncionarioService, private readonly route: ActivatedRoute, private readonly router: Router){}
+
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.funcionarioService.GetFuncionarioId(this.id).subscribe(response => {
+      
+      const dados = response.dados;
+
+      dados.dataDeCriacao = new Date(dados.dataDeCriacao!).toLocaleDateString('pt-BR');
+
+      dados.dataDeAlteracao = new Date(dados.dataDeAlteracao!).toLocaleDateString('pt-BR');
+      
+      this.funcionario = response.dados; 
+    })
+  }
+
+  inativaFuncionario() {
+    this.funcionarioService.InativaFuncionario(this.id).subscribe(response => {
+      this.router.navigate(['/']);
+    })
+  }
+}
