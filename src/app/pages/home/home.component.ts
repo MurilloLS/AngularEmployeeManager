b8aba92b24a845114c889ee2004db1b9ed6b2483
Murillo Santos <murillo.lds01@gmail.com>
2024-10-05 20:26:02 -0300

    feat: add dynamic router links

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FuncionarioService } from '../../services/funcionario.service';
 import { Funcionarios } from '../../models/Funcionarios';
 import { RouterModule } from '@angular/router';
-import { response } from 'express';
 
 
 @Component({
