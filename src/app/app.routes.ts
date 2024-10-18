204232623c9b2173f86bb9a3235d4146dc17b53c
Murillo Santos <murillo.lds01@gmail.com>
2024-10-18 16:13:08 -0300

    refactor(routing): update login route to '/' and home route to '/home'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { EditarComponent } from './pages/editar/editar.component';
 import { DetalhesComponent } from './pages/detalhes/detalhes.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-  { path: 'cadastro', component: CadastroComponent },
-  { path: '', component: HomeComponent },
-  { path: 'editar/:id', component: EditarComponent },
-  { path: 'detalhes/:id', component: DetalhesComponent}
+  { path: 'cadastro', component: CadastroComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'editar/:id', component: EditarComponent, canActivate: [AuthGuard] },
+  { path: 'detalhes/:id', component: DetalhesComponent, canActivate: [AuthGuard] },
+  { path: '', component: LoginComponent },
 ];
