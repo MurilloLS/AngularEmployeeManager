a83d62b6bbed50b55edaf7288e707091d583e95d
Murillo Santos <murillo.lds01@gmail.com>
2024-10-19 18:48:33 -0300

    feat(auth): add authorization headers to FuncionarioService API requests

--- src/app/services/funcionario.service.ts
+++ src/app/services/funcionario.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Funcionarios } from '../models/Funcionarios';
 import { Response } from '../models/Response';
@@ -12,34 +12,39 @@ export class FuncionarioService {
 
   ApiUrl = `${environment.UrlApi}/Funcionario`;
 
-  constructor(private readonly http : HttpClient) { }
-  
-  GetFuncionarios() : Observable<Response<Funcionarios[]>> {
-    return this.http.get<Response<Funcionarios[]>>(this.ApiUrl);
+  constructor(private readonly http: HttpClient) { }
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
   }
 
-  CriarFuncionario(funcionario: Funcionarios) : Observable<Response<Funcionarios[]>> {
-    return this.http.post<Response<Funcionarios[]>>(`${this.ApiUrl}`, funcionario);
+  GetFuncionarios(): Observable<Response<Funcionarios[]>> {
+    return this.http.get<Response<Funcionarios[]>>(this.ApiUrl, { headers: this.getAuthHeaders() });
   }
 
-  DeletarFuncionario(id: number): Observable<Response<Funcionarios[]>>{
-    return this.http.delete<Response<Funcionarios[]>>(`${this.ApiUrl}?id=${id}`)
+  CriarFuncionario(funcionario: Funcionarios): Observable<Response<Funcionarios[]>> {
+    return this.http.post<Response<Funcionarios[]>>(`${this.ApiUrl}`, funcionario, { headers: this.getAuthHeaders() });
   }
 
-  GetFuncionarioId(id: number): Observable<Response<Funcionarios>>{
-    return this.http.get<Response<Funcionarios>>(`${this.ApiUrl}/${id}`);
+  DeletarFuncionario(id: number): Observable<Response<Funcionarios[]>> {
+    return this.http.delete<Response<Funcionarios[]>>(`${this.ApiUrl}?id=${id}`, { headers: this.getAuthHeaders() });
   }
 
-  EditarFuncionario(funcionario: Funcionarios): Observable<Response<Funcionarios[]>>{
-    return this.http.put<Response<Funcionarios[]>>(`${this.ApiUrl}`, funcionario);
+  GetFuncionarioId(id: number): Observable<Response<Funcionarios>> {
+    return this.http.get<Response<Funcionarios>>(`${this.ApiUrl}/${id}`, { headers: this.getAuthHeaders() });
   }
 
-  InativaFuncionario(id: number): Observable<Response<Funcionarios[]>>{
-    return this.http.put<Response<Funcionarios[]>>(`${this.ApiUrl}/InativaFuncionario/${id}`, id);
+  EditarFuncionario(funcionario: Funcionarios): Observable<Response<Funcionarios[]>> {
+    return this.http.put<Response<Funcionarios[]>>(`${this.ApiUrl}`, funcionario, { headers: this.getAuthHeaders() });
   }
 
-  AtivaFuncionario(id: number): Observable<Response<Funcionarios[]>>{
-    return this.http.put<Response<Funcionarios[]>>(`${this.ApiUrl}/AtivaFuncionario/${id}`, id);
+  InativaFuncionario(id: number): Observable<Response<Funcionarios[]>> {
+    return this.http.put<Response<Funcionarios[]>>(`${this.ApiUrl}/InativaFuncionario/${id}`, id, { headers: this.getAuthHeaders() });
+  }
+
+  AtivaFuncionario(id: number): Observable<Response<Funcionarios[]>> {
+    return this.http.put<Response<Funcionarios[]>>(`${this.ApiUrl}/AtivaFuncionario/${id}`, id, { headers: this.getAuthHeaders() });
   }
 
 }
